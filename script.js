d1349c77570c2ccbe058d8a6ef5b33fd9d60332b
sudo-quinnmaloney <64997686+sudo-quinnmaloney@users.noreply.github.com>
2021-03-20 00:20:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
+
 function jump() {
   character.classList.add("jumping")
+}
+
+functino slide() {
+  block.classList.add("sliding")
 }
