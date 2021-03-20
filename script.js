567ad23bbf8b4d447f06de2addd312a477e08f93
sudo-quinnmaloney <64997686+sudo-quinnmaloney@users.noreply.github.com>
2021-03-20 01:11:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,13 @@ var checkCollisions = setInterval(function(){
   var characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
   var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
   if (Math.abs(blockLeft - characterLeft) <= 20 && characterTop >= 125) {
-    
+    if (character.classList == "jumping") {
+      character.classList.remove("jumping");
+    } else {
+      character.classList.remove("falling");
+    }
+  }
+  else if (character.classList != "jumping" && characterTop <= 125) {
+    character.classList.add("falling");
   }
 },10);
