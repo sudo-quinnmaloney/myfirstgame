ccd368a031fcec6add333a0430ef681877aa6553
sudo-quinnmaloney <64997686+sudo-quinnmaloney@users.noreply.github.com>
2021-03-20 00:53:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,17 @@ var character = document.getElementById("character");
 var block = document.getElementById("block");
 
 function jump() {
-  character.classList.add("jumping")
+  if (characater.classList != "jumping"){
+    character.classList.add("jumping");
+  }
+  setTimeOut(function() {character.classList.remove("jumping");},1000);
 }
+
+var checkCollisions = setInterval(function(){
+  var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+  var characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+  var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+  if (-15 <= blockLeft - characterLeft <= 20 && characterTop >= 125) {
+    alert("Contact");
+  }
+},10);
