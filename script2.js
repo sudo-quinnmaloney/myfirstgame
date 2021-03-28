0c28a94d0a6593c93e641b7844d62aef97e7e534
sudo-quinnmaloney <64997686+sudo-quinnmaloney@users.noreply.github.com>
2021-03-28 01:14:42 -0700

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,1 +1,9 @@
+  
+var character = document.getElementById("character");
+var block = document.getElementById("block");
 
+function jump {
+  if (character.classList.contains("jumper")){return}
+  character.classList.add("jumper");
+  setTimeOut(jump {character.classList.remove("jumper");},1000);
+}
