5e44939ca929b4938b9ed02d533313362f60fea5
sudo-quinnmaloney <64997686+sudo-quinnmaloney@users.noreply.github.com>
2021-03-28 01:57:33 -0700

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,7 +1,6 @@
   
 var character = document.getElementById("character");
 var block = document.getElementById("block");
-var game = document.getElementById("block");
 
 function jump() {
   if (character.classList.contains("jumper")){return;}
